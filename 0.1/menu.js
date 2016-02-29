e7120652443a85df66623a65d1463352c98db84c
JaysenDraney <draneyj@gmail.com>
2016-02-28 23:51:08 -0800

    Update menu.js

--- 0.1/menu.js
+++ 0.1/menu.js
@@ -5,11 +5,30 @@
 
 function createMenu(options){
   
+  function newIcon(iconClass){
+    return $('<span/>').addClass('ui-icon ' + iconClass);
+  }
+  
+  var baseCss  = { 
+    "background"  : "none",
+    "color"       : "black",
+    "top"         : "0px",
+    "z-index"     : "1000",
+    "position"    : "fixed"
+  };
+  
+  var $base = $('<div/>').attr('id','tampermenubase').css(baseCss);
+  
+  
+  function toggeExpand(){
+    $base.toggleClass('expanded'); 
+  }
 
-  var $expander = $('<button/>').attr('id', 'amazonFreeStyleExpander')
-  .text("Jaysend's Amazon FreeStyle")
+  var $expander = $('<button/>')
+  .attr('id', 'amazonFreeStyleExpander')
+  .text(options.textName)
   .button({
-    icons: { primary: "ui-icon-gear" },
+    icons: { primary: "ui-arrowthickstop-1-e" },
     text: false
   })
   .click(function(){
