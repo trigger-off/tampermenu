a7e208c2e42652558ac21612fe232bf0b5af6dd0
JaysenDraney <draneyj@gmail.com>
2016-02-28 23:44:06 -0800

    Update menu.js

--- 0.1/menu.js
+++ 0.1/menu.js
@@ -1,1 +1,19 @@
+/*global 
+  GM_addStyle, $
+*/
+'use strict';
 
+function createMenu(options){
+  
+
+  var $expander = $('<button/>').attr('id', 'amazonFreeStyleExpander')
+  .text("Jaysend's Amazon FreeStyle")
+  .button({
+    icons: { primary: "ui-icon-gear" },
+    text: false
+  })
+  .click(function(){
+    expand();
+  });
+  
+}
